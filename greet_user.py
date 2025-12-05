b0bb3d971dddc27a8b3d26fb4b1e5211fabe0de2
Talha Ahmad <tahmad.bscs25seecs@seecs.edu.pk>
2025-12-05 12:17:25 +0500

    Add three Python functions

---
+++ greet_user.py
@@ -0,0 +1,4 @@
+def greet(name):
+    return(f"Hello {name}!")
+Name=input("Enter your name : ")
+print(greet(Name))
---
+++ multiply.py
@@ -0,0 +1,5 @@
+def multiply(a,b):
+    return(a*b)
+N1=int(input("Enter first number : "))
+N2=int(input("Enter second number : "))
+print(f"Product of {N1} & {N2} is {multiply(N1,N2)} .")
